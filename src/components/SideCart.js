573c57c9cfdc90a230440aae674954ca477046e1
Sergey Skupow <sergskypow@gmail.com>
2019-09-19 16:26:32 +0300

    image fix

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -16,7 +16,7 @@ export default function SideCart() {
               {cart.map(item=>{
                 return(
                   <li key={item.id} className="cart-item mb-4">
-                    <img width="35" src={`../${item.image}`} alt="item img"/>
+                    <img width="35" src={item.image} alt="item img"/>
                     <div className="mt-3">
                       <h6 className="text-uppercase">{item.title}</h6>
                       <h6 className="text-title text-capitilize">amount : {item.count}</h6>
--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -30,7 +30,7 @@ export default function SingleProductPage() {
                 <div className="row">
                   <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
                     <img
-                      src={`../${image}`}
+                      src={image}
                       alt="product img"
                       className="img-fluid"
                     />
