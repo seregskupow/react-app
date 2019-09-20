a729f436ce3776ed1194e9ec0d078e611d56a2f7
Sergey Skupow <sergskypow@gmail.com>
2019-09-20 13:46:06 +0300

    paypall added

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -3,11 +3,11 @@ import CartSection from '../components/CartPage'
 import CartImg from '../images/storeBcg.jpeg'
 import Hero from '../components/Hero'
 
-export default function CartPage() {
+export default function CartPage(props) {
   return (
     <>
     <Hero img={CartImg}/>
-    <CartSection></CartSection>
+    <CartSection history={props.history}></CartSection>
 </>
   );
 }
